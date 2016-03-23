9a484b983586c1c4a9c0a289c39c227c235d4d54
Ken Hu <silvesthu@gmail.com>
2016-03-24 03:45:33 +0900

    initial commit

---
+++ RayTracingWeekend/RayTracingWeekend.cpp
@@ -0,0 +1,12 @@
+// RayTracingWeekend.cpp : Defines the entry point for the console application.
+//
+
+#include <tchar.h>
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	return 0;
+}
+
